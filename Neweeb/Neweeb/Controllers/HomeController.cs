9356b904bc90e5397938ffbd831ab2b8b4112e98
HZiver <bd7hskl2t@code.edu.az>
2024-05-06 19:48:47 +0400

    davam ele

---
+++ Neweeb/Neweeb/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Neweeb.Models;
+
+namespace Neweeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Customer customer = new Customer()
+            {
+                Fullname="Vladimir Putin",
+                Comment="Veli hardadir? onu gozleryirem rusyetde",
+                Profilphoto="download.jpeg"
+            };
+            Customer customer1 = new Customer()
+            {
+                Fullname = "Haschbullah Magomedovich Magomedov",
+                Comment = "Fight me!!!",
+                Profilphoto="hasbullah.jpeg"
+            };
+            List<Customer> customers = new List<Customer>();
+            customers.Add(customer);
+            customers.Add(customer1);
+            return View(customers);
+        }
+    }
+}
---
+++ Neweeb/Neweeb/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Neweeb.Models
+{
+    public class Customer
+    {
+      public int Id { get; set; }
+      public string Fullname { get; set; }
+      public string Profilphoto { get; set; }
+        public string Comment { get; set; }
+    }
+}
